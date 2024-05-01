7befa47a4e02aa878e00538611392f1fcee602f9
Dennis Aldana <dennis.aldana@gmail.com>
2024-04-30 20:11:35 -0600

    add: testing configs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@hooks': '/src/hooks'
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './setupTests.js'
+  }
 })
